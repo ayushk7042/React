21baa601f6cfcece204dda0f7d2de1ddc8102ded
Ayush <ayushkayush1@gmail.com>
2025-07-26 17:21:10 +0530

    3

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,8 +3,11 @@ import {useState} from "react";
 export default function Counter(){
     let [count, setCount] = useState(0);
 
+    console.log(`count =${count}`);   // console.log ( count issue fix)
+
     let intCount = () => {
         setCount(count +1);
+        console.log(`inside incount, count =${count}`);  // console.log ( count issue fix)
     };
     return (
         <div>
