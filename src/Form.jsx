27e704b9df19bc32dd80593bbaefa42b512572c1
Ayush <ayushkayush1@gmail.com>
2025-07-31 20:37:20 +0530

    mulipleform

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,14 +1,44 @@
 import { useState} from "react";
 
 export default function Form() {
-    let[fullName, setFullName] = useState('ayush');
+    let [formData, setFormData] = useState({
+        fullName: "",
+        username: "",
+        password: "",
+    })
 
-    let handleNameChange = (event) => {
-        setFullName(event.target.value);
+    let handleInputChange = (event) => {
+        setFormData((currData) => {
+            return { ...currData, [event.target.name]: event.target.value};
+        });
+    }
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: "",
+            username: "",
+            password: "",
+        })
     }
 
     return (
-        <form > <input placeholder="enter full name" type= "text" value= {fullName} onChange= {handleNameChange}/>
+  <form onSubmit= {handleSubmit}>
+            <label htmlfor="fullName">username</label>
+             <input placeholder="enter username " type= "text" value= {formData.fullName} id= "fullName" name= "fullName" onChange= {handleInputChange} />
+        
+
+
+        
+            <label htmlFor="username">username</label>
+             <input placeholder="enter username " type= "text" value= {formData.username} id= "username" name= "username" onChange= {handleInputChange} />
+        
+
+        
+            <label htmlFor="password">Password</label>
+             <input placeholder="enter password" type= "password" value= {formData.password} id= "password" name= "password"
+              onChange= {handleInputChange} />
         <button> submit</button> </form>
     )
 }
